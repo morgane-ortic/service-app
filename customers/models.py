fe195b57b3dee9e9dfb983e8d070a389a20442c3
Morgane Ortic <morgane.ortic@proton.me>
2024-11-28 11:27:26 +0100

    created Customer model

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+# The user model doesn't need to be defined because it's built in Django
+# We import it here to use some of the fields in the Customer model
 
-# The user model doesn't need to be defined because it's built-in
-# We import it to use some of the fields in the Customer model
+class Customer(models.Model):
+
+    GENDER_CHOICE =  [
+        ('W', 'Woman'),
+        ('M', 'Man'),
+        ('O', 'Other'),
+    ]
+
+    # Import User fields to use them as Customer fields
+    # customers will be deleted automatically when the associated user is
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE), null=True
+    description = models.CharField(1000), null=True
+    picture = models.ImageField(upload_to='customer_pictures/'), null=True
+    # We'll use the Pillow library to upload + store images and use the image fields
+
+    def __str__(self):
+        return self.user.username
+
+# This overrides the delete method to also delete the associated user
+    def delete(self, *args, **kwargs):
+        user = self.user
+        super().delete(*args, **kwargs)
+        user.delete()
