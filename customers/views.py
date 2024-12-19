c9d970e7f7772aa5a063d34bb05189bd08e4ef68
Morgane Ortic <morgane.ortic@proton.me>
2024-12-19 14:57:55 +0100

    set up temporary base + home templates with test customer views and basic css

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Placeholder views
+def home(request):
+    return render(request, 'customers/home.html')
+
+def services(request):
+    return render(request, 'customers/services.html')
+
+def bookings(request):
+    return render(request, 'customers/bookings.html')
+
+def profile(request):
+    return render(request, 'customers/profile.html')
+
+def about(request):
+    return render(request, 'customers/about.html')
+
+def contact(request):
+    return render(request, 'customers/contact.html')
+
+def register(request):
+    return render(request, 'customers/register.html')
+
+def login(request):
+    return render(request, 'customers/login.html')
--- massageservice/settings.py
+++ massageservice/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Used to access css file directly in the static folder
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
