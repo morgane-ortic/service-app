c51708e069740f32d50e840eb5c3a0b0309d9bc9
Morgane Ortic <morgane.ortic@proton.me>
2025-01-13 15:59:48 +0100

    implemented basic therapist personal + professional profile

--- therapists/views.py
+++ therapists/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm, RegisterDetailsForm
+from .forms import RegisterForm, PersonalDetailsForm, ProDetailsForm, EmptyForm
 from core.forms import LoginForm
 from .models import Therapist
 
@@ -31,10 +31,35 @@ def about(request):
     })
 
 def profile(request, section='personal_details'):
+    therapist = get_object_or_404(Therapist, user=request.user)
+
+    if request.method == 'POST':
+        if section == 'personal_details':
+            personal_form = PersonalDetailsForm(request.POST, request.FILES, instance=therapist)
+            if personal_form.is_valid():
+                personal_form.save()
+        elif section == 'professional_details':
+            pro_form = ProDetailsForm(request.POST, instance=therapist)
+            if pro_form.is_valid():
+                pro_form.save()
+
+    else:
+        if section == 'personal_details':
+            personal_form = PersonalDetailsForm(instance=therapist)
+            pro_form = None
+        elif section == 'professional_details':
+            pro_form = ProDetailsForm(instance=therapist)
+            personal_form = None
+        else:
+            personal_form = None
+            pro_form = None
+
     return render(request, 'therapists/profile.html', {
         'base_template': 'therapists/base.html',
-        'section': section
-        })
+        'section': section,
+        'personal_form': personal_form,
+        'pro_form': pro_form
+    })
 
 def register(request):
     if request.method == 'POST':
@@ -58,16 +83,29 @@ def register(request):
 @login_required
 def register_details(request):
     if request.method == 'POST':
-        form = RegisterDetailsForm(request.POST, request.FILES)  # Pass request.FILES to handle image upload
-        if form.is_valid():
-            therapist = form.save(commit=False)  # Save the form but don't commit to the database yet
-            therapist.user = request.user  # Assuming you want to link the therapist to the logged-in user
-            therapist.save()  # Now save the therapist instance
-            form.save_m2m()  # Save the many-to-many relationships
+        personal_form = PersonalDetailsForm(request.POST, request.FILES)
+        pro_form = ProDetailsForm(request.POST)
+        
+        if personal_form.is_valid() and pro_form.is_valid():
+            # Save the personal details form
+            therapist = personal_form.save(commit=False)
+            therapist.user = request.user 
+            therapist.save()
+            personal_form.save_m2m()  # Save the many-to-many relationships
+            
+            # Save the professional details form
+            pro_details = pro_form.save(commit=False)
+            pro_details.therapist = therapist  # Link the professional details to the therapist
+            pro_details.save()
             return redirect('therapists:register_confirm')  # Redirect after saving
     else:
-        form = RegisterDetailsForm()
-    return render(request, 'therapists/register_details.html', {'form': form})
+        personal_form = PersonalDetailsForm()
+        pro_form = ProDetailsForm()
+    return render(request, 'therapists/register_details.html', {
+        'personal_form': personal_form,
+        'pro_form': pro_form,
+        'empty_form': EmptyForm
+    })
 
 
 def register_confirm(request):
