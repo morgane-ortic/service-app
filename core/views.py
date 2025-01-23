05604c31c29977f3d0df5ba991ba95fc4bc1d81f
Morgane Ortic <morgane.ortic@proton.me>
2025-01-23 12:04:07 +0100

    implemented basic notification display view + therapist template

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 from django.http import JsonResponse
-from core.models import Booking
+from core.models import Booking, Notification
 
 
 def user_login(request):
@@ -32,3 +33,20 @@ def cancel_booking(request, booking_id):
         return JsonResponse({"success": True, "html": render_to_string("booking_history_item.html", {"booking": booking})})
 
     return JsonResponse({"success": False, "error": "Invalid request method"}, status=400)
+
+
+@login_required
+def display_notifications(request):
+    user = request.user
+    notifications = Notification.objects.filter(recipient=user).order_by('-created_at')
+
+    # fetch notifications sent to all local therapists
+    if hasattr(user, 'therapist'):
+        therapist = user.therapist
+        city_notifications = Notification.objects.filter(city=therapist.city)
+        notifications = notifications.union(city_notifications).order_by('-created_at')
+        template_name = 'therapists/notifications.html'
+    else:
+        template_name = 'customers/notifications.html'
+
+    return render(request, template_name, {'notifications': notifications})
--- therapists/urls.py
+++ therapists/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+import core.views
 
 app_name = 'therapists'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('notifications/', views.notifications, name='notifications'),
+    path('notifications/', core.views.display_notifications, name='notifications'),
     path('schedule/', views.schedule, name='schedule'),
     
     path('profile/<str:section>/', views.profile, name='profile'),
